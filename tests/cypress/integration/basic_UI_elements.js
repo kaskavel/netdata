a178e36abe3665552c4031c5d031a0125b1bbb47
tzoker <joethliv@gmail.com>
2021-06-18 11:34:00 +0300

    Syntax changes for readability

--- tests/cypress/integration/basic_UI_elements.js
+++ tests/cypress/integration/basic_UI_elements.js
@@ -9,16 +9,16 @@ context('Assertions', () => {
     it('.should() - Assert functionality of basic elements', () => {
 
       	// date picker
-      	cy.get('div.styled__AccessorBox-sc-1yj3701-8').click();
+		cy.get('div.styled__AccessorBox-sc-1yj3701-8').click();
       	cy.get('[data-testid="date-picker::click-quick-selector::::7200"]').click();
       	cy.get('[data-testid="date-picker::click-apply::-start"]').click();
       	cy.get('div.styled__AccessorBox-sc-1yj3701-8').contains('Last 2 hours');
 	
-	// Theme changer
-	cy.get('[data-target="#optionsModal"][title="Settings"]').click();
-	cy.get('[data-toggle="tab"][role="tab"]').contains('Visual').click();
+		// Theme changer
+		cy.get('[data-target="#optionsModal"][title="Settings"]').click();
+		cy.get('[data-toggle="tab"][role="tab"]').contains('Visual').click();
       	cy.contains('Dark').click({force: true});
-	cy.get('div#loadOverlay').should('have.css', 'color', 'rgb(221, 221, 221)')
+		cy.get('div#loadOverlay').should('have.css', 'color', 'rgb(221, 221, 221)')
     })
   })
 
